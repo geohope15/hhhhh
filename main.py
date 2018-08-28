13569d664ed4eab9c4ba6ed4223baff8ec388d78
geohoperepresents@gmail.com <geohoperepresents@gmail.com>
2018-08-28 12:44:07 +0530

    add

--- main.py
+++ main.py
@@ -91,5 +91,9 @@ def convert5():
 def firstpage():
     return render_template("firstpage.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
